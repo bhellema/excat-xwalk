be9aa6a7ff51787335eefa937d974ab5b7a2f578
Ben Helleman <bhellema@adobe.com>
2025-12-10 07:57:04 -0500

    remove all blocks

--- blocks/hero-abbvie/hero-abbvie.js
+++
@@ -1,4 +0,0 @@
-export default function decorate(block) {
-  // Hero-abbvie variant - minimal decoration
-  // Background image and content handled by CSS
-}
--- blocks/hero-video-promo/hero-video-promo.js
+++
@@ -1,3 +0,0 @@
-export default function decorate(block) {
-  // Hero video promo block - no additional decoration needed
-}
